5223cb60eace5c8ab2d2b8d631e1eae0365d85a0
Joris Bastiaans <joris.bastiaans@nl.steinweg.com>
2024-12-02 13:19:30 +0100

    Added day01, although it was completed with Excel at first...

--- day01/day01.py
+++ day01/day01.py
@@ -1,1 +1,28 @@
-print("bruh, use Excel...")
+from typing import List
+
+
+with open("day01/input.txt", "r") as f:
+    left: List[int] = list()
+    right: List[int] = list()
+
+    for (l, r) in [ l.split("   ") for l in f.readlines() ]:
+        left.append(int(l))
+        right.append(int(r))
+
+    right.sort()
+    left.sort()
+
+
+def part1(l:List[int], r:List[int]) -> int:
+    return sum(map(lambda x: abs(x[0]-x[1]), zip(l, r)))
+
+def part2(l:List[int], r:List[int]) -> int:
+    return sum(map(lambda x: x[0] * r.count(x[0]), zip(l, r)))
+
+
+print(f"""
+    
+    Part 1 = {part1(left, right)}
+    Part 2 = {part2(left, right)}
+
+""")
